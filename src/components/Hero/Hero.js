bdaa8396a758525bb3db946ec3d77a323e07b1de
Tamara <tamara.northe@laparkan.com>
2023-02-15 13:49:54 -0400

    Modifed the components of some jsx files

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br/>
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpose of this page is to help developers build better more modern appicaltions for users and clients
+      </SectionText>
+      <Button onClick={() => window.location = 'http://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,17 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider/>
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map((project) =>(
+        <div>
+          {project.title}
+        </div>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
